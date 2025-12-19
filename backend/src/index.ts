381e120a10d7277248530ae208e8a794f07c6696
Shildeburger <edushander@gmail.com>
2025-12-19 11:13:53 +0300

    Added endpoint in index.ts and fixed tsconfig

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 
+const gamemods = [
+  { key: 'classic', name: 'Classic', description: 'classic' },
+  { key: 'timed', name: 'Timed', description: 'timed' },
+  { key: 'challenge', name: 'Challenge', description: 'challenge' },
+];
 const expressApp = express();
 expressApp.get('/ping', (req, res) => {
   res.send('pong');
+});
+expressApp.get('/gamemods', (req, res) => {
+  res.send(gamemods);
 });
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000');
