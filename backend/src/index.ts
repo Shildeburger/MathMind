92f0aae4ff8e70babe1b311c400887ac8d9ab190
Shildeburger <edushander@gmail.com>
2025-12-19 12:18:53 +0300

    added tRPC in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trps';
 
-const gamemods = [
-  { key: 'classic', name: 'Classic', description: 'classic' },
-  { key: 'timed', name: 'Timed', description: 'timed' },
-  { key: 'challenge', name: 'Challenge', description: 'challenge' },
-];
 const expressApp = express();
 expressApp.get('/ping', (req, res) => {
   res.send('pong');
 });
-expressApp.get('/gamemods', (req, res) => {
-  res.send(gamemods);
-});
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+);
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000');
 });
---
+++ backend/src/trps.ts
@@ -0,0 +1,15 @@
+import { initTRPC } from '@trpc/server';
+
+const trpc = initTRPC.create();
+
+const gamemods = [
+  { key: 'classic', name: 'Classic', description: 'classic' },
+  { key: 'timed', name: 'Timed', description: 'timed' },
+  { key: 'challenge', name: 'Challenge', description: 'challenge' },
+];
+
+export const trpcRouter = trpc.router({
+  getGameMode: trpc.procedure.query(() => {
+    return { gamemods };
+  }),
+});
