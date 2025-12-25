21e6fe0231234a6552f021716d4104807815df32
Shildeburger <edushander@gmail.com>
2025-12-25 17:09:03 +0300

    Finished frontend tRPC. Created monorep

--- webapp/src/pages/GamemodsPage/index.tsx
+++ webapp/src/pages/GamemodsPage/index.tsx
@@ -1,12 +1,23 @@
 import { trpc } from '../../lib/trpc';
 
 export const GamemodsPage = () => {
-  const result = trpc.getGameMode.useQuery();
-  console.log(result);
-  return gamemods.map((mod) => (
-    <div key={mod.key}>
-      <h2>{mod.name}</h2>
-      <p>{mod.description}</p>
+  const { data, error, isLoading, isFetching, isError } = trpc.getGameMode.useQuery();
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <div>
+      <h1>Game Mods</h1>
+      {data?.gamemods.map((mod) => (
+        <div key={mod.key}>
+          <h2>{mod.name}</h2>
+          <p>{mod.description}</p>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
